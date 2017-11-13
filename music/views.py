8083ce2cc596a25582cfcde1a182bc572dc3e904
Yi Ji <jiyi12345@Gmail.com>
2017-11-13 16:16:27 +0000

    Add detail in view

--- music/views.py
+++ music/views.py
@@ -3,3 +3,8 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>This is the music app homepage</h1>")
+
+
+def detail(request, album_id):
+    return HttpResponse("<h1>Details for Album ID: " + str(album_id) + "</h1>")
+
