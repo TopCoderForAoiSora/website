fdd1b9995640c9f4566337e41916ce518d98e55c
Yi Ji <jiyi12345@Gmail.com>
2017-11-13 23:05:46 +0000

    using template

--- music/views.py
+++ music/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import Album
 
 
 def index(request):
-    return HttpResponse("<h1>This is the music app homepage</h1>")
+    all_albums = Album.objects.all()
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums': all_albums,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, album_id):
