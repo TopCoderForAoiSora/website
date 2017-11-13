cfb095e6c2e527c0af5b7f9831a985a9e2cb049b
520aoisora <520aoisora@gmail.com>
2017-11-13 23:28:48 +0000

    create detail page

--- music/views.py
+++ music/views.py
@@ -1,17 +1,17 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.http import Http404
+from django.shortcuts import render
 from .models import Album
 
 
 def index(request):
     all_albums = Album.objects.all()
-    template = loader.get_template('music/index.html')
-    context = {
-        'all_albums': all_albums,
-    }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'music/index.html', {'all_albums': all_albums})
 
 
 def detail(request, album_id):
-    return HttpResponse("<h1>Details for Album ID: " + str(album_id) + "</h1>")
+    try:
+        album = Album.objects.get(pk = album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album doesn't exist!")
+    return render(request, 'music/detail.html', {'album':album})
 
