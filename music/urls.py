6a845f81b9d82cd69851aa909ee465d126f63236
520aoisora <520aoisora@gmail.com>
2017-11-15 08:57:57 +0000

    choose favourite song

--- music/urls.py
+++ music/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import url
 from . import views
 
+
+app_name = 'music'
+
 urlpatterns = [
     # /music/
     url(r'^$', views.index, name='index'),
 
     # /music/79/
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+
+    # /music/79/favourite
+    url(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name='favourite'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Album
+from .models import Album, Song
 
 
 def index(request):
@@ -11,3 +11,17 @@ def detail(request, album_id):
     album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
 
+
+def favourite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/details.html', {
+            'album': album,
+            'error_message': 'You did not select a valid song'
+        })
+    else:
+        selected_song.is_favourite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
