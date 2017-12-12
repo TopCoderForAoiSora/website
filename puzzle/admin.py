396bd0532afaf206ead182156ff37a901ebb53ac
Yi Ji <jiyi12345@Gmail.com>
2017-12-12 17:32:57 +0000

    Admin page refactory

--- puzzle/admin.py
+++ puzzle/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import Puzzle, PlayerGameHistory
+from django.contrib.admin.templatetags.admin_list import admin_list_filter
 
-admin.site.register(Puzzle)
-admin.site.register(PlayerGameHistory)
+class PuzzleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'location', 'point', 'content')
+
+
+admin.site.register(Puzzle, PuzzleAdmin)
+
+
+class PlayerGameHistoryAdmin(admin.ModelAdmin):
+    list_display = ('user', 'score')
+    list_filter = ['score']
+
+
+admin.site.register(PlayerGameHistory, PlayerGameHistoryAdmin)
